21e4af8b3de84dc26fac79e60b52b7210e5221f2
Samir55 <discere044@icloud.com>
2018-03-30 22:34:23 +0200

    Some Refactoring.

---
+++ Java/src/search/engine/ranker/Main.java
@@ -0,0 +1,10 @@
+package search.engine.ranker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ranker r = new Ranker();
+        r.run("Input/erdos-10000.txt");
+    }
+
+}
